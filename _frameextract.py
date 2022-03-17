b0c62db0de42326cec567f658a71e3bf08999363
Asher Merrill <asher.s.merrill@gmail.com>
2022-03-16 20:34:10 -0600

    frame loading: add cuda image encode
    * also add option for media type

--- _frameextract.py
+++ _frameextract.py
@@ -19,34 +19,37 @@ def build_command(finput, foutput):
     command = list()
     command.extend([
         r'ffmpeg.exe',  # symlink to (static!) ffmpeg in local dir
-        r'E:\Shared\Downloads\ffmpeg-n5.0-latest-win64-gpl-5.0\bin\ffmpeg.exe',
+        r'-hwaccel_output_format',
+        r'cuda',
         r'-i',
         finput,
         r'-r',
         r'1/1',  # force 1 fps input to 1 fps output
         r'-vf',
-        r'scale=512:512',  # need larger frame size for feature detection (maybe)
+        # need larger frame size for feature detection (maybe)
+        r'scale=512:512',
         foutput,
     ])
 
     return command
 
 
-def main(pathtomovie=''):
+def main(pathtomovie='', mediatype=False):
     print('=' * 80)
     print(f'\tLooking in:\t\t\t{os.path.dirname(pathtomovie)}')
     print('=' * 80)
 
-    fname = glob.glob(pathtomovie)
+    fname = glob.glob(pathtomovie, recursive=True)
 
     for f in fname:
         outputsubdir = os.path.join(
-            OUTPUTDIR, os.path.basename(os.path.splitext(f)[0]))
+            OUTPUTDIR, 'anime' if mediatype is True else 'photo',
+            os.path.basename(os.path.splitext(f)[0]))
         if not os.path.exists(outputsubdir):
             os.mkdir(outputsubdir)
 
         command = build_command(
-            f, os.path.join(outputsubdir, '%05d.jpg'))
+            f, os.path.join(outputsubdir, OUTPUTCODE))
 
         with subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True, shell=True) as p:
             for line in p.stdout:
@@ -61,5 +64,7 @@ if __name__ == '__main__':
     )
     parser.add_argument('pathtomovie', type=str,
                         help='glob string for movie name')
+    parser.add_argument('-a', '--anime', dest='mediatype', action='store_true',
+                        default=False, help='input media type')
     kwargs = vars(parser.parse_args())
     main(**kwargs)
